f0b673b9f228313ed34f7ff93927cf9409d2cfaf
Akira Kondo <3p4owi@gmail.com>
2026-03-14 11:41:26 +0900

    feat: Initialize Tauri project configuration, set up React entry point, and manage fonts using `@fontsource`.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/700.css'
 import './index.css'
 import App from './App.tsx'
 import { applyFonts } from './lib/fontOptions'
